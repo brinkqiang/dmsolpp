3b0ec2be884f0be797992d2d1f17477748c1e1bd
brinkqiang <brink.qiang@gmail.com>
2021-11-08 12:55:16 +0800

    add

--- test/dmsolpptest/main.cpp
+++ test/dmsolpptest/main.cpp
@@ -74,3 +74,68 @@ TEST(DoSolModule, DoSolModule)
         )", sol::script_throw_on_error);
     ASSERT_TRUE(script_result.valid());
 }
+
+TEST(DoSol, DoSol)
+{
+    CDMLuaEngine oDMLuaEngine;
+
+    std::string strScriptRootPath = DMGetRootPath();
+    oDMLuaEngine.SetRootPath(strScriptRootPath + PATH_DELIMITER_STR + ".." + PATH_DELIMITER_STR + "script");
+
+    oDMLuaEngine.AddModule(require_interface);
+
+    if (!oDMLuaEngine.ReloadScript())
+    {
+        ASSERT_TRUE(0);
+        return;
+    }
+
+    oDMLuaEngine.DoString(R"(
+        local interface = require("interface")
+        local p = interface.CPlayer.new()
+        p:Init()
+        p:NotChange()
+        p.OnChange = function (self) print("OnChange in lua") end
+        p:OnChange()
+        print(MAX_NAME_LEN)
+        print("[1]" .. interface.GNextID())
+        print("[2]" .. p.NextID())
+        p:SetObjID(interface.GNextID())
+        print("[3]" .. p:GetObjID())
+        p:SetHP(interface.GNextID())
+        print("[4]" .. p:GetHP())
+        print("[5]" .. interface.CPlayer.NextID())
+        )");
+
+    oDMLuaEngine.DoString(R"(
+        function add(a , b)
+            return a + b
+        end
+        )");
+
+    auto state = oDMLuaEngine.GetSol();
+
+    int num = state["add"](1, 2);
+
+    int num2 = oDMLuaEngine.CallT<int>("add", 1, 2);
+
+    CPlayer oPlayer;
+    int num3 = state["test"]["main"]["dowork"](&oPlayer);
+
+    auto script_result = state.safe_script(R"(
+        local interface = require("interface")
+
+        local p = interface.CPlayer.new()
+        p:Init()
+        p:NotChange()
+        p:OnChange()
+        print("[1]" .. interface.GNextID())
+        print("[2]" .. p.NextID())
+        p:SetObjID(interface.GNextID())
+        print("[3]" .. p:GetObjID())
+        p:SetHP(interface.GNextID())
+        print("[4]" .. p:GetHP())
+        print("[5]" .. interface.CPlayer.NextID())
+        )", sol::script_throw_on_error);
+    ASSERT_TRUE(script_result.valid());
+}
