02b500ede3ad1777231b7605cdfb7460cfa734b7
brinkqiang <brink.qiang@gmail.com>
2024-12-30 22:44:42 +0800

    add

--- test/dmsolpptest/common/macros.h
+++ test/dmsolpptest/common/macros.h
@@ -2,13 +2,13 @@
 #ifndef __MACROS_H_INCLUDE__
 #define __MACROS_H_INCLUDE__
 
-// tolua_begin
+// export_begin
 #define MAX_HP  99999
 #define MAX_MP  99999
 
 #define MAX_ROLE_COUNT 10
 #define MAX_NAME_LEN 64
-// tolua_end
+// export_end
 
 #endif // __MACROS_H_INCLUDE__
 
